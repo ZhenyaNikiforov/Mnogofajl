92505922475e3e957f44da5cc64a132d934d32b1
Zhenya Nikiforov <12A92@mail.ru>
2025-10-10 19:33:29 +0500

    Второе задание исправил

--- Vtoroe_zadanie_class/classCounter.h
+++ Vtoroe_zadanie_class/classCounter.h
@@ -1,5 +1,5 @@
-#include <iostream>
 #pragma once
+#include <iostream>
 using namespace std;
 
 class Counter
--- Vtoroe_zadanie_class/main.cpp
+++ Vtoroe_zadanie_class/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "main.h"
+#include "./classCounter.h"
 using namespace std;
 
 int main()
